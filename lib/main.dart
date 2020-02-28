2aa69b18f2bf07dc99f64f475805914273abb4f6
camilaborinsky <cborinsky@itba.edu.ar>
2020-02-28 14:19:49 -0300

    lista de quotes, string simples

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,35 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp();
+void main() => runApp(MaterialApp(
+  home: QuotesList()
+));
+
+class QuotesList extends StatefulWidget {
+  @override
+  _QuotesListState createState() => _QuotesListState();
+}
+
+class _QuotesListState extends State<QuotesList> {
+
+  List<String> quotes = [
+    "Nothing changes if nothing changes",
+    "The best view comes after the hardest climb",
+    "It is during our darkest moments we must focus on the light",
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Awesome Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote) => Text(quote)).toList(),//map devuelve un iterable
+      ),
+    );
+  }
+}
+
 
